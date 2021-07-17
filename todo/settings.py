0f1c659ac337e4603357a0c6ee3a7d5915053a35
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-07-17 15:57:16 +0100

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ogre!&(5)md&d%2@3r*jk&ue$vqi1^h9qyn(^)^i@ya5xnppwv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trackmemo.herokuapp.com','127.0.0.1']
 
 
 # Application definition
