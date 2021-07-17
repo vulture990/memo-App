ed77806779704eed653745e3c12adfbc76691121
vulture990 <mohamedsali602@live.com>
2021-07-17 14:02:00 +0100

    fixed

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
